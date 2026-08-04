3413b4ecd8aa70497a49bb6be56e1003ff33f02c
Srd kumar <sharadkumar7725@gmail.com>
2026-08-04 16:11:17 +0530

    array basics

---
+++ Arrays/Create.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Create{
+    public static void main(String args[]){
+        int arr[] = {34, 3, 4, 3, 2, 4, 23};
+
+        System.out.println(arr.length);
+        for(int i = 0; i< arr.length-1; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
