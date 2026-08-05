24e2306597d4c3bc9c10e481711f8c483b32f8e1
Srd kumar <sharadkumar7725@gmail.com>
2026-08-05 21:37:40 +0530

    secondLargeEle

---
+++ Arrays/SecondLarge.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class SecondLarge{
+    public static void main(String args[]){
+
+        int arr[] = {22, 2, 4, 33, 56, 23, 67, 90, 89};
+        Arrays.sort(arr);
+      
+        System.out.print(arr[arr.length-2]);
+
+    }
+}
