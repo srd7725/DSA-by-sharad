9ae8ca299709b6eb28155fe5629a827d13f210e9
Srd kumar <sharadkumar7725@gmail.com>
2026-08-16 17:02:32 +0530

    encapsulation

---
+++ Oops/encapsulation/Main4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Stud {
+    private int marks = 99;
+    public int getMarks(){
+        return marks;
+    }
+}
+
+public class Main4 {
+    public static void main(String args[]){
+        Stud s = new Stud();
+        // System.out.println(s.getMarks());
+       System.out.println( s.getMarks());
+    }
+}
