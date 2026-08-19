067a8117918dff8022d173a8e7a6b03ee60bbd47
Srd kumar <sharadkumar7725@gmail.com>
2026-08-19 18:24:30 +0530

    basic print str

---
+++ String/BasicStr.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class BasicStr{
+    public static void main (String args[]){
+        String s = "sharad live in indore";
+        System.out.println("String is : "+ s);
+    }
+}
