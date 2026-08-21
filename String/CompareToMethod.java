4edbd362e3e7b15d0dd4952c70d30fd626cc28c2
Srd kumar <sharadkumar7725@gmail.com>
2026-08-22 01:03:31 +0530

    concat method

---
+++ String/CompareToMethod.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class CompareToMethod{
+    public static void main(String args[]){
+       String a = "sameerart";
+       String b = "sameer";
+       System.out.print(a.compareTo(b)); 
+    }
+}
---
+++ String/Concat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Concat{
+    public static int compareTo(String a, String b){
+
+        return -1;
+    }
+    public static void main(String args[]){
+        String a = "srd";
+        String b = "golu";
+        a = a.concat(b);
+        System.out.println(a);
+    }
+}
